6d4d9174fccd251b7c3fba6b2d785de39271885e
Gangesh Matta <gangeshmatta@gmail.com>
2018-11-17 20:44:36 +0530

    Update config.js
    Endpoint Update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-let isProd = process.env.NODE_ENV === 'production';
+let isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  REST_ENDPOINT: JSON.stringify('https://wptavern.com/wp-json/wp/v2'),
+  REST_ENDPOINT: JSON.stringify("https://slidearts.com/wp-json/wp/v2"),
   POSTS_PER_PAGE: JSON.stringify(9),
-  GA_TRACKING_ID: JSON.stringify(isProd ? 'UA-111438908-1' : 'XXX'),
+  GA_TRACKING_ID: JSON.stringify(isProd ? "UA-111438908-1" : "XXX"),
   REQUEST_CACHE_MAX: JSON.stringify(150)
 };
